9c2c11e0d5b830b1c96719da28d4e4e1b308c032
Sero <kmcaltiz@gmail.com>
2024-11-28 17:57:36 +0800

    add middlewares and routers for api

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world!");
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import UserRouter from "./routers/user/router.js";
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+//routers
+app.use("/v1/api", UserRouter);
+
+app.get("/v1/api", (req, res) => {
+  res.send({ message: "API is working fine." });
+});
+
+app.listen(process.env.PORT, () => console.log("Server started"));
