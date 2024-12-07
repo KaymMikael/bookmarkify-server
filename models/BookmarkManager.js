dc2d425b7f94d74fd49e7678756c4730445e485b
Sero <kmcaltiz@gmail.com>
2024-12-07 15:34:35 +0800

    create getbyid and editbookmarkbyid function

--- models/BookmarkManager.js
+++ models/BookmarkManager.js
@@ -14,6 +14,7 @@ class BookmarkManager {
     const [rows] = await pool.query(query, [bookmark_url, userId]);
     return rows[0].count > 0;
   }
+
   async insertBookMark(values) {
     const [user_id, bookmark_title, bookmark_url, is_public] = values;
     if (await this.#bookmarkExists(bookmark_url, user_id)) {
@@ -38,8 +39,9 @@ class BookmarkManager {
     const [result] = await pool.query(query, [userId]);
 
     if (!result.length) {
-      return { message: "No bookmark found" };
+      return { message: "Bookmark not found" };
     }
+
     return result.map((data) => ({
       bookmarkId: data.bookmark_id,
       user_id: data.user_id,
@@ -64,7 +66,7 @@ class BookmarkManager {
     const [result] = await pool.query(query);
 
     if (!result.length) {
-      return { message: "No bookmark found" };
+      return { message: "Bookmark not found" };
     }
 
     return result.map((data) => ({
@@ -119,7 +121,7 @@ class BookmarkManager {
 
     //Return this if there are no bookmark
     if (!result.length) {
-      return { message: "No bookmark found" };
+      return { message: "Bookmark not found" };
     }
 
     return result.map((data) => ({
@@ -133,8 +135,30 @@ class BookmarkManager {
       tags: data.tags ? data.tags.split(",") : [],
     }));
   }
+
+  async getById(bookmarkId) {
+    const query = `SELECT b.bookmark_id AS bookmark_id, b.bookmark_url, b.bookmark_title, b.is_public, b.user_id, GROUP_CONCAT(t.tag_name) AS tags, b.created_at
+                  FROM bookmarks b 
+                  JOIN bookmark_tags bt ON b.bookmark_id = bt.bookmark_id 
+                  JOIN tags t ON bt.tag_id = t.tag_id
+                  WHERE b.bookmark_id = ?
+                  GROUP BY b.bookmark_id`;
+    const [result] = await pool.query(query, [bookmarkId]);
+    if (!result.length) {
+      throw new Error("Bookmark not found");
+    }
+    return result;
+  }
+
+  async editBookmarkById(values) {
+    const qery =
+      "UPDATE bookmarks SET bookmark_title = ?, bookmark_url = ?, is_public = ? WHERE bookmark_id = ?";
+    const [result] = await pool.query(query, values);
+    return result;
+  }
 }
 
 const bookMarkInstance = new BookmarkManager();
 
 export default bookMarkInstance;
+
